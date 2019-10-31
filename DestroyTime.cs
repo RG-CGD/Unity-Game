4070493d788577df1ddba5e483439c5bfc513e4a
Ryan McMahon <b00358095@studentmail.uws.ac.uk>
2019-10-31 11:07:53 +0000

    Destroy Time Script

---
+++ DestroyTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTime : MonoBehaviour
+{
+    public int destroyTimer = 5;
+
+    private void Awake()
+    {
+        Destroy(gameObject, destroyTimer);
+    }
+}
