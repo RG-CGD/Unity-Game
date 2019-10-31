c801d146f43aa1eaf7291e7141ce8ebf820a0ae5
Ryan McMahon <b00358095@studentmail.uws.ac.uk>
2019-10-31 11:06:33 +0000

    Player Health Script
    Code for the player health in the game

---
+++ PlayerHealth.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public static PlayerHealth singleton;
+    public float currentHealth;
+    public float maxHealth = 100f;
+    public Slider healthSlider;
+    public Text healthCounter;
+    public bool isDead = false;
+    [Header("Damage Screen")]
+    public Color damageColor;
+    public Image damageImage;
+    float colorSmoothing = 6f;
+    bool isTakingDamage = false;
+
+    private void Awake()
+    {
+        singleton = this;
+    }
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthSlider.value = maxHealth;
+        UpdateHealthUI();
+    }
+
+    private void Update()
+    {
+        if(isTakingDamage)
+        {
+            damageImage.color = damageColor;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, colorSmoothing * Time.deltaTime);
+        }
+        isTakingDamage = false;
+    }
+
+
+
+
+    public void PlayerDamage(float damage)
+    {
+        if(currentHealth > 0)
+        {
+            if (damage >= currentHealth)
+            {
+                isTakingDamage = true;
+                Dead();
+            }
+            else
+            {
+                isTakingDamage = true;
+                currentHealth -= damage;
+            }
+            UpdateHealthUI();
+        }
+    }
+    public void AddHealth(float healthAmount)
+    {
+        currentHealth += healthAmount;
+        UpdateHealthUI();
+    }
+
+    void Dead()
+    {
+        currentHealth = 0;
+        isDead = true;
+        healthSlider.value = 0;
+        UpdateHealthUI();
+        Debug.Log("Player Is Dead");
+    }
+
+    void UpdateHealthUI()
+    {
+        healthCounter.text = currentHealth.ToString();
+        healthSlider.value = currentHealth;
+    }
+}
